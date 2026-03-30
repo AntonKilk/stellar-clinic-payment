e26befae43191f7057c1e9d31d0f8ef2d1cb1d64
Anton Kilk <ant.kilk@gmail.com>
2026-03-30 12:04:32 +0300

    Issue-2: Add checkStyle. Edit initial code to sucessfully build with check

--- inquiry-service/src/main/java/com/stellar/crm/inquiryservice/InquiryServiceApplication.java
+++ inquiry-service/src/main/java/com/stellar/crm/inquiryservice/InquiryServiceApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InquiryServiceApplication {
 
-    public static void main(String[] args) {
+    private InquiryServiceApplication() {
+    }
+
+    public static void main(final String[] args) {
         SpringApplication.run(InquiryServiceApplication.class, args);
     }
 
