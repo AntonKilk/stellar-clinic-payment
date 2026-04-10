c09316b0684f389d80ce469537da95e6b591fb4c
Anton Kilk <ant.kilk@gmail.com>
2026-04-10 16:49:41 +0300

    Issue-3: remove unique indentifier for group ref id because no need to check it here

--- inventory-service/src/main/resources/db/changelog/changes/001-create-groups-table.sql
+++ inventory-service/src/main/resources/db/changelog/changes/001-create-groups-table.sql
@@ -1,14 +1,14 @@
 -- liquibase formatted sql
 
 -- changeset inventory-service:001-create-groups-table
-CREATE TABLE groups
-(
-    guid                    UUID            PRIMARY KEY,
-    group_ref_id            UUID            NOT NULL UNIQUE,
-    current_count           integer         NOT NULL DEFAULT 0,
-    group_limit             integer         NOT NULL,
-    created_at              TIMESTAMPTZ     NOT NULL,
-    updated_at              TIMESTAMPTZ     NOT NULL
-);
+    CREATE TABLE groups
+    (
+        guid                    UUID            PRIMARY KEY,
+        group_ref_id            UUID            NOT NULL,
+        current_count           integer         NOT NULL DEFAULT 0,
+        group_limit             integer         NOT NULL,
+        created_at              TIMESTAMPTZ     NOT NULL,
+        updated_at              TIMESTAMPTZ     NOT NULL
+    );
 
 -- rollback DROP TABLE groups;
