dbd18483d6f42cac1753ebe1d7d0123cbfb2d810
Anton Kilk <ant.kilk@gmail.com>
2026-04-27 06:36:01 +0300

    Issue-7: add actuator in customer service

---
+++ customer-service/src/main/java/com/stellar/crm/customerservice/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.stellar.crm.customerservice.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi customersApi() {
+        return GroupedOpenApi.builder()
+                .group("customers")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi actuatorApi() {
+        return GroupedOpenApi.builder()
+                .group("actuator")
+                .pathsToMatch("/actuator/**")
+                .build();
+    }
+}
